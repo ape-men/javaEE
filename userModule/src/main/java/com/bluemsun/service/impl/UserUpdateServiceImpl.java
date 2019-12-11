9d2644a1b67b9d7f821ae647ec4420911dc64884
tenforests <434856668@qq.com>
2019-12-11 22:57:41 +0800

    update

---
+++ userModule/src/main/java/com/bluemsun/service/impl/UserUpdateServiceImpl.java
@@ -0,0 +1,28 @@
+package com.bluemsun.service.impl;
+
+import com.bluemsun.enums.UserStateEnum;
+import com.bluemsun.service.UserUpdateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserUpdateServiceImpl implements UserUpdateService {
+    @Override
+    public UserStateEnum updateUser() {
+        return null;
+    }
+
+    @Override
+    public Integer updatePassword() {
+        return null;
+    }
+
+    @Override
+    public Integer findPassword() {
+        return null;
+    }
+
+    @Override
+    public Integer submitRegister() {
+        return null;
+    }
+}
---
+++ userModule/src/main/java/com/bluemsun/web/FrontedController.java
@@ -0,0 +1,41 @@
+package com.bluemsun.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FrontedController {
+//    @RequestMapping(value = "/signIn/signIn", method = RequestMethod.GET)
+//    private String login() {
+//        return "signIn/signIn";
+//    }
+//    @RequestMapping(value ="/register/register",method = RequestMethod.GET)
+//    private String register(){
+//        return "register/register";
+//    }
+//    @RequestMapping(value = "/publish/publish",method = RequestMethod.GET)
+//    private String publish(){
+//        return "publish/publish";
+//    }
+//    @RequestMapping(value = "/profile/profile",method = RequestMethod.GET)
+//    private String profile(){
+//        return "profile/profile";
+//    }
+//    @RequestMapping(value = "/messageManage/messageManage",method = RequestMethod.GET)
+//    private String messageManage(){
+//        return "messageManage/messageManage";
+//    }
+//    @RequestMapping(value = "/index/index",method = RequestMethod.GET)
+//    private String index(){
+//        return "index/index";
+//    }
+//    @RequestMapping(value = "/grades/grades",method = RequestMethod.GET)
+//    private String grades(){
+//        return "grades/grades";
+//    }
+//    @RequestMapping(value = "/answerSheet/answerSheet",method = RequestMethod.GET)
+//    private String answerSheet(){
+//        return "answerSheet/answerSheet";
+//    }
+}
